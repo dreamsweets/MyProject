c2eb661cea49c61eecb90d29587e4ba4b1282cb2
dreamsweets <33395001+dreamsweets@users.noreply.github.com>
2019-03-13 17:09:42 +0900

    Add Solution
    GameLoop정리

---
+++ MyProjectPrototype/MyProjectPrototype/main.cpp
@@ -0,0 +1,20 @@
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+class PC;
+class NPC;
+
+int main() {
+	//Initialization 초기화
+	PC player;
+	NPC enemy;
+	//게임 루프 구성 
+	while (/*escape*/) {
+		Input//입력
+		Update();//오브젝트들 업뎃해야할 것 다 업데이트
+		render();//렌더링
+	}
+
+	//Decommissioning
+	return 0;
+}
