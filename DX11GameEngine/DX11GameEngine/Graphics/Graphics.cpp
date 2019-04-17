c663d81ece5a36713dbe870f1d033cdd779ed085
dreamsweets <33395001+dreamsweets@users.noreply.github.com>
2019-04-18 01:37:53 +0900

    Font 출력 테스트 및 윈도우크기 fix

--- DX11GameEngine/DX11GameEngine/Graphics/Graphics.cpp
+++ DX11GameEngine/DX11GameEngine/Graphics/Graphics.cpp
@@ -33,6 +33,10 @@ void Graphics::RenderFrame()
 	this->deviceContext->IASetVertexBuffers(0, 1, vertexBuffer.GetAddressOf(), &stride, &offset);
 	this->deviceContext->Draw(3, 0);
 	
+	//Draw Text
+	spriteBatch->Begin();
+	spriteFont->DrawString(spriteBatch.get(), L"HELLO FUCKING WORLD", DirectX::XMFLOAT2(0, 0), DirectX::Colors::White, 0.0f, DirectX::XMFLOAT2(0.0f, 0.0f), DirectX::XMFLOAT2(1.0f, 1.0f));
+	spriteBatch->End();
 	this->swapchain->Present(1, NULL);
 
 }
@@ -171,6 +175,8 @@ bool Graphics::InitializeDirectX(HWND hwnd, int width, int height)
 		ErrorLogger::Log(hr, "Failed to create rasterizer state.");
 		return false;
 	}
+	spriteBatch = std::make_unique<DirectX::SpriteBatch>(this->deviceContext.Get());
+	spriteFont = std::make_unique<DirectX::SpriteFont>(this->device.Get(), L"Data\\Fonts\\comic_sans_ms_16.spritefont");
 	return true;
 }
 
